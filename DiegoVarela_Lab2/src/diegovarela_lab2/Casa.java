7ac3d34afd4e8b097a36933b517ed58f1f9ad5a3
Diego Varela <diegocrash124@gmail.com>
2020-05-08 14:33:22 -0600

    Se finaliza lo que es crear y listar casa

---
+++ DiegoVarela_Lab2/src/diegovarela_lab2/Casa.java
@@ -0,0 +1,176 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diegovarela_lab2;
+
+import java.util.ArrayList;
+
+public class Casa {
+    private int numcasa;
+    private int numblock;
+    private String color;
+    private double ancho;
+    private double largo;
+    private String comprada;
+    private int numpiso;
+    private int numbaño;
+    private int numcuarto;
+    private String dueño;
+    private String estado;
+    private String ingeniero;
+    private ArrayList construccion=new ArrayList();
+    private ArrayList lista=new ArrayList();
+    private ArrayList espera=new ArrayList();
+    private ArrayList demolicion=new ArrayList();
+
+    public Casa(int numcasa, int numblock, String color, double ancho, double largo, String comprada, int numpiso, int numbaño, int numcuarto, String dueño, String estado, String ingeniero) {
+        this.numcasa = numcasa;
+        this.numblock = numblock;
+        this.color = color;
+        this.ancho = ancho;
+        this.largo = largo;
+        this.comprada = comprada;
+        this.numpiso = numpiso;
+        this.numbaño = numbaño;
+        this.numcuarto = numcuarto;
+        this.dueño = dueño;
+        this.estado = estado;
+        this.ingeniero = ingeniero;
+    }
+
+    public int getNumcasa() {
+        return numcasa;
+    }
+
+    public void setNumcasa(int numcasa) {
+        this.numcasa = numcasa;
+    }
+
+    public int getNumblock() {
+        return numblock;
+    }
+
+    public void setNumblock(int numblock) {
+        this.numblock = numblock;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+
+    public double getLargo() {
+        return largo;
+    }
+
+    public void setLargo(double largo) {
+        this.largo = largo;
+    }
+
+    public String getComprada() {
+        return comprada;
+    }
+
+    public void setComprada(String comprada) {
+        this.comprada = comprada;
+    }
+
+    public int getNumpiso() {
+        return numpiso;
+    }
+
+    public void setNumpiso(int numpiso) {
+        this.numpiso = numpiso;
+    }
+
+    public int getNumbaño() {
+        return numbaño;
+    }
+
+    public void setNumbaño(int numbaño) {
+        this.numbaño = numbaño;
+    }
+
+    public int getNumcuarto() {
+        return numcuarto;
+    }
+
+    public void setNumcuarto(int numcuarto) {
+        this.numcuarto = numcuarto;
+    }
+
+    public String getDueño() {
+        return dueño;
+    }
+
+    public void setDueño(String dueño) {
+        this.dueño = dueño;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getIngeniero() {
+        return ingeniero;
+    }
+
+    public void setIngeniero(String ingeniero) {
+        this.ingeniero = ingeniero;
+    }
+
+    public ArrayList getConstruccion() {
+        return construccion;
+    }
+
+    public void setConstruccion(ArrayList construccion) {
+        this.construccion = construccion;
+    }
+
+    public ArrayList getLista() {
+        return lista;
+    }
+
+    public void setLista(ArrayList lista) {
+        this.lista = lista;
+    }
+
+    public ArrayList getEspera() {
+        return espera;
+    }
+
+    public void setEspera(ArrayList espera) {
+        this.espera = espera;
+    }
+
+    public ArrayList getDemolicion() {
+        return demolicion;
+    }
+
+    public void setDemolicion(ArrayList demolicion) {
+        this.demolicion = demolicion;
+    }
+
+    @Override
+    public String toString() {
+        return "Casa{" + "comprada=" + comprada + ", due\u00f1o=" + dueño + ", estado=" + estado + ", ingeniero=" + ingeniero + '}';
+    }
+    
+}
--- DiegoVarela_Lab2/src/diegovarela_lab2/DiegoVarela_Lab2.java
+++ DiegoVarela_Lab2/src/diegovarela_lab2/DiegoVarela_Lab2.java
@@ -5,17 +5,140 @@
  */
 package diegovarela_lab2;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 /**
  *
  * @author diego
  */
 public class DiegoVarela_Lab2 {
 
-    /**
-     * @param args the command line arguments
-     */
+    static Scanner entrada = new Scanner(System.in);
+    static boolean in = false;
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        ArrayList<Casa> casas = new ArrayList();
+        char resp = 's';
+
+        while (resp == 's') {
+            System.out.println("Registro de casas");
+            System.out.println("Manejo de estados");
+            System.out.println("Log in");
+            System.out.println("Salir");
+            int enter = entrada.nextInt();
+            switch (enter) {
+                case 1:
+                    if (in = true) {
+                        System.out.println("1) Crear Casas");
+                        System.out.println("2) Listar Casas");
+                        System.out.println("3) Modificar Casas");
+                        System.out.println("4) Borrar Casas");
+                        System.out.println("Ingrese la opcion que desea");
+                        int ent = entrada.nextInt();
+                        switch (ent) {
+                            case 1:
+                                System.out.println("Ingrese el numero de casa:");
+                                int numcasa = entrada.nextInt();
+                                System.out.println("Ingrese el numero de bloque:");
+                                int numbloque = entrada.nextInt();
+                                System.out.println("Ingrese el color de la casa:");
+                                String col = entrada.next();
+                                System.out.println("Ingrese el ancho de la casa:");
+                                double anc = entrada.nextDouble();
+                                System.out.println("Ingrese el largo de la casa:");
+                                double lar = entrada.nextDouble();
+                                System.out.println("Ingrese si ya fue comprada(Si o no):");
+                                String comp = entrada.next();
+                                System.out.println("Ingrese el numero de pisos:");
+                                int numpiso = entrada.nextInt();
+                                System.out.println("Ingrese el numero de baños:");
+                                int numb = entrada.nextInt();
+                                String dueño = "";
+                                if (comp.equals("Si") || comp.equals("si")) {
+                                    System.out.println("Ingrese el nombre del dueño:");
+                                    dueño = entrada.next();
+                                }
+                                System.out.println("Ingrese el nkombre del inegienero a cargo:");
+                                String ing = entrada.next();
+                                System.out.println("Ingrese el estado de la casa(solo minusculas):");
+                                String estado = entrada.next();
+                                casas.add(new Casa(numcasa, numbloque, col, anc, lar, comp, numpiso, numb, numcasa, dueño, estado, ing));
+                                break;
+                            case 2:
+                                for (Casa c : casas) {
+                                    System.out.println(c);
+                                }
+                                break;
+                            case 3:
+                                for (Casa c : casas) {
+                                    System.out.println(c);
+                                }
+                                System.out.println("Ingrese la posiocion de la casa que desea modificar:");
+                                int mod = entrada.nextInt();
+                                System.out.println("¿Que desea modificar?");
+                                System.out.println("1) Numero de casa");
+                                System.out.println("2) Numero de bloque");
+                                System.out.println("3) Color");
+                                System.out.println("4) Ancho");
+                                System.out.println("5) Largo");
+                                System.out.println("6) Si ya fue comprada");
+                                System.out.println("7) Numero de pisos");
+                                System.out.println("8) Numero de baños");
+                                System.out.println("9) Numero de cuartos");
+                                System.out.println("10) El nombre del dueño");
+                                System.out.println("11) Ingeniero a crago");
+                                switch () {
+                                    case 1:
+                                        break;
+                                    case 2:
+                                        break;
+                                    case 3:
+                                        break;
+                                    case 4:
+                                        break;
+                                    case 5:
+                                        break;
+                                    case 6:
+                                        break;
+                                    case 7:
+                                        break;
+                                    case 8:
+                                        break;
+                                    case 9:
+                                        break;
+                                    case 10:
+                                        break;
+                                    case 11:
+                                        break;
+
+                                }
+                                break;
+                            case 4:
+                                break;
+                        }
+                    } else {
+                        System.out.println("No se puede entrar usuario o contraseña no valido");
+                    }
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    System.out.println("Ingrese su usuario:");
+                    String us = entrada.next();
+                    System.out.println("Ingrese su contraseña:");
+                    String cont = entrada.next();
+                    if (us.equals("leobanegas") && cont.equals("99")) {
+                        in = true;
+                    }
+                    break;
+
+                case 4:
+                    resp = 'n';
+                    break;
+            }
+
+        }
     }
-    
+
 }
